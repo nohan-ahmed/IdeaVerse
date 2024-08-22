ea1c9c70eec4471185260b521fe2fd00168b4f2b
Nohan ahmed <mdpolashislam152@gmail.com>
2024-08-22 19:48:29 +0600

    Add alternative_text field on image model

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Follow, UserInfo
 
 
 # Register your models here.
@@ -7,3 +7,19 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "username", "email", "is_staff", "is_superuser", "is_active"]
     search_fields = ["id", "username", "gender", "email", "phone"]
+    
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ["id",'user', "location", "study_at", "created_at"]
+    search_fields = ["id",'user', "location", "study_at", "created_at"]
+    
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ["id", "follower", "following", "created_at"]
+    search_fields = ["id", "follower", "following", "created_at"]
+    
+    def follwer(self, obj):
+        return obj.follower
+    
+    def following(self, obj):
+        return obj.following
