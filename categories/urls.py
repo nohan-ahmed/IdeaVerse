129a8085da0b6993e547884195e38ac9204c7223
Nohan ahmed <mdpolashislam152@gmail.com>
2024-08-27 17:52:42 +0600

    Add search opation for categories

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/', views.CategoryAPIView.as_view(), name='category')
+    path('', views.CategoryAPIView.as_view(), name='category')
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Category
 from .serializers import CategorySerializer
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
 # from rest_framework.permissions import 
 # Create your views here.
 
-class CategoryAPIView(RetrieveAPIView):
+class CategoryAPIView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['slug', 'id']
+    
